03dfbc29e52b36b4e80a72eb46d0fd6ff60074bc
yassirrchi <yassirrchi7@gmail.com>
2022-03-30 16:15:08 +0000

    src

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
 import React from 'react';
+import FilterBox from './Components/FilterBox'
+ 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Home from './HomePage/Home';
  
 function App() {
   return (
 <React.Fragment>
 
-  <Home/>
-    </React.Fragment>
+<BrowserRouter>
+
+<Routes>
+
+  <Route path="/" element={<Home/> }/>
+  <Route path="/filterbox" exact element={<div><Home/><FilterBox/></div> }/>
+  <Route path="/filterboxo" exact element={<div><Home/><FilterBox/>boxo</div> }/>
+ 
+
+</Routes>
+
+ </BrowserRouter> 
+
+  </React.Fragment>
     
     
   );
